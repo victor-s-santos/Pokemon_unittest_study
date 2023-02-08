ffded7eee8458d927321b22853b1636695ace25d
Victor <victor.santos.cd@gmail.com>
2023-02-08 18:12:49 -0300

    test(pokemon.tests.test_pokemon): Write the first tests schema

--- pokemon/tests/test_pokemon.py
+++ pokemon/tests/test_pokemon.py
@@ -3,10 +3,12 @@ from pokemon import (
     get_pokemon_types,
     get_evolution_line,
     get_amount_of_pokemons_same_gen,
+    get_evolution_line,
+    insert_pokemon_in_database,
 )
 
 
-class TestPokemon(unittest.TestCase):
+class TestPokemonFormat(unittest.TestCase):
     def setUp(self):
         self.pikachu = "pikachu"
         self.expected_list_types = [
@@ -34,6 +36,21 @@ class TestPokemon(unittest.TestCase):
         """Must return a list instance."""
         list_types = get_pokemon_types(self.pikachu)
         self.assertIsInstance(list_types, list)
+
+    def test_get_amount_of_pokemons_same_gen_format(self):
+        """Must return a int instance."""
+        amount_of_pokemons = get_amount_of_pokemons_same_gen(self.pikachu)
+        self.assertIsInstance(amount_of_pokemons, int)
+
+    def test_get_evolution_line_format(self):
+        """Must return a list instance."""
+        evolution_line = get_evolution_line(self.pikachu)
+        self.assertIsInstance(evolution_line, list)
+
+    def test_insert_pokemon_in_database(self):
+        """Must return a string instance."""
+        insert_pokemon_in_database_obj = insert_pokemon_in_database(self.pikachu)
+        self.assertIsInstance(insert_pokemon_in_database_obj, str)
 
 
 if __name__ == "__main__":
