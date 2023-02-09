9ef3a68d9ac7f222f0856fd5df1591d3dd2a151d
Victor <victor.santos.cd@gmail.com>
2023-02-08 21:54:29 -0300

    refactor(pokemon): Refactor code using class Pokemon instead functions

--- pokemon/pokemon.py
+++ pokemon/pokemon.py
@@ -10,48 +10,53 @@ logging.basicConfig(
 )
 
 
-def get_pokemon_types(name: str) -> list:
-    """Returns a list of types names for the given pokemon name.
+class Pokemon:
+    def __init__(self, name):
+        self.name = name
 
-    Args:
-        name (str): The name of pokemon to be searched.
+    def get_requests(self):
+        """Realize the requests.get"""
+        r = requests.get(f"https://pokeapi.glitch.me/v1/pokemon/{name}")
+        return r.json()
 
-    Returns:
-        list: A list of types names of respective pokemon
-    """
-    r = requests.get(f"https://pokeapi.glitch.me/v1/pokemon/{name}")
-    return r.json()[0]["types"]
+    def get_pokemon_types(self) -> list:
+        """Returns a list of types names for the given pokemon name.
 
+        Args:
+            name (str): The name of pokemon to be searched.
 
-def get_amount_of_pokemons_same_gen(name: str) -> int:
-    """Returns the amount of pokemons in the same gen as the given pokemon name.
+        Returns:
+            list: A list of types names of respective pokemon
+        """
+        return self.get_requests()[0]["types"]
 
-    Args:
-        name (str): The name of pokemon to be searched.
+    def get_amount_of_pokemons_same_gen(self) -> int:
+        """Returns the amount of pokemons in the same gen as the given pokemon name.
 
-    Returns:
-        int: The amount of pokemons in the same gen.
-    """
-    r = requests.get(f"https://pokeapi.glitch.me/v1/pokemon/{name}")
-    n_gen = r.json()[0]["gen"]
+        Args:
+            name (str): The name of pokemon to be searched.
 
-    r = requests.get(f"https://pokeapi.glitch.me/v1/pokemon/counts")
-    return r.json()[f"gen{n_gen}"]
+        Returns:
+            int: The amount of pokemons in the same gen.
+        """
+        r = requests.get(f"https://pokeapi.glitch.me/v1/pokemon/{name}")
+        n_gen = r.json()[0]["gen"]
 
+        r = requests.get(f"https://pokeapi.glitch.me/v1/pokemon/counts")
+        return r.json()[f"gen{n_gen}"]
 
-def get_evolution_line(name: str) -> list:
-    """Returns the list of the evolution line for the given pokemon name.
+    def get_evolution_line(self) -> list:
+        """Returns the list of the evolution line for the given pokemon name.
 
-    Args:
-        name (str): The name of pokemon to be searched.
+        Args:
+            name (str): The name of pokemon to be searched.
 
-    Returns:
-        list: The list of pokemon names in the evolution line for the given pokemon name.
-    """
-    r = requests.get(f"https://pokeapi.glitch.me/v1/pokemon/{name}")
-    family = r.json()[0]["family"]
-    evolution_line = family["evolutionLine"]
-    return evolution_line
+        Returns:
+            list: The list of pokemon names in the evolution line for the given pokemon name.
+        """
+        family = self.get_requests()[0]["family"]
+        evolution_line = family["evolutionLine"]
+        return evolution_line
 
 
 def insert_pokemon_in_database(name: str) -> str:
